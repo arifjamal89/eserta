908c349ba17e8333670c089c536f5d99b029ff2f
arifjamal89 <myadmin@WP-BPM-M3.modnet.mindef.my>
2018-10-30 17:04:47 +0800

    CRUD Complete

--- penganjur/urls.py
+++ penganjur/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
 	path('', views.home, name='home_penganjur'),
 	#http://127.0.0.1:8000/penganjur/aktiviti/add/
 	path('aktiviti/add/', views.addaktiviti, name='add_aktiviti'),
+	path('aktiviti/<int:pk>/edit/', views.editaktiviti, name='edit_aktiviti'),
+	path('aktiviti/<int:pk>/delete', views.delete_aktiviti, name='delete_aktiviti'),
 ]
--- penganjur/views.py
+++ penganjur/views.py
@@ -1,21 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import Aktiviti
 from .forms import AktivitiForm
+from django.urls import reverse_lazy
+
 # Create your views here.
 
 # Home penganjur
 def home(request):
 
 	# aktivitiid = request.GET['aktivitiid']
-	print(request.GET['aktivitiid'])				#sama macam get php id
+	#print(request.GET['aktivitiid'])				#sama macam get php id
 
 	#List of record
 	a = Aktiviti.objects.all()						#semua object/data letak dalam a
-	#print(a.tajuk)
-	for ak in a:									#Papar data di cmd
-	 	print (ak.tajuk,ak.tempat,ak.penceramah)
+	#print(a.tajuk)   
+	# for ak in a:									#Papar data di cmd
+	#  	print (ak.tajuk,ak.tempat,ak.penceramah)
 
-	return render(request,'penganjur/home.html') 
+	return render(request,'penganjur/home.html',{'aktiviti': a}) 
 
 #update aktiviti
 def update_penganjur(request,pk):
@@ -52,10 +54,57 @@ def delete_penganjur(request,pk):
 def addaktiviti(request):
 
 	if request.method == "POST":
-		pass # temporary kosong 
+		
+		form = AktivitiForm(request.POST) 			#value akan submit ke form
+
+		if form.is_valid(): 								#validate semak semua dah isi
+			aktiviti = form.save(commit=False) 				#simpan dalam memori
+			aktiviti.save() 								#save ke db
+			return redirect(reverse_lazy('home_penganjur'))
+		#print("POST DAH MASUK") 							#ini macam echo nak semak masuk POST ke x
+
+
 	else:
 		form= AktivitiForm()
+		#print(form)
 
 	return render(request,'penganjur/tambahaktiviti.html',{'form' : form})
 
+#hapus aktiviti guna form
+def delete_aktiviti(request,pk):
 
+	aktiviti = get_object_or_404(Aktiviti,pk=pk)
+
+	if request.method == 'POST':
+
+		if request.POST.get("submit_yes"):
+			print(request.method)
+			aktiviti.delete()
+			return redirect(reverse_lazy('home_penganjur'))
+
+	#View page confirm delete	
+	return render(request,'penganjur/confirm_delete_aktiviti.html',{'aktiviti':aktiviti})
+
+#edit aktiviti forms py
+def editaktiviti(request,pk):
+
+	#Dapatkan rekod berdasarkan pk yang dihantar
+	aktiviti= get_object_or_404(Aktiviti,pk=pk)
+
+
+	if request.method == "POST":
+		
+		form = AktivitiForm(request.POST,instance=aktiviti) #value akan submit ke form
+
+		if form.is_valid(): 								#validate semak semua dah isi
+			aktiviti = form.save(commit=False) 				#simpan dalam memori
+			aktiviti.save() 								#save ke db
+			return redirect(reverse_lazy('home_penganjur'))
+		#print("POST DAH MASUK") 							#ini macam echo nak semak masuk POST ke x
+
+
+	else:
+		form= AktivitiForm(instance=aktiviti) #yang ni dapatkan id untuk papar
+		#print(form)
+
+	return render(request,'penganjur/editaktiviti.html',{'form' : form})
